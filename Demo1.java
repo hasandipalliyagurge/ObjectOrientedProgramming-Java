5c08adb56257d0ee3802be4113222c5a6ae48456
hasandi_palliaguruge <pghasandi@gmail.com>
2019-12-23 20:10:11 +0530

    objects

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+class Box{
+	int length;
+	int width;
+	int height;
+}
+class Demo1{
+	public static void main(String args[]){
+		Box b1=new Box();
+		System.out.println("length : "+b1.length);
+		System.out.println("width  : "+b1.width);
+		System.out.println("height : "+b1.height);
+		
+	}
+}
