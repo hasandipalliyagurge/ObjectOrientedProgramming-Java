0380e8634ffa6fa0ff6e53e58777a2351d7a3f88
hasandi_palliaguruge <pghasandi@gmail.com>
2019-12-23 20:02:34 +0530

    constructors

---
+++ Cal.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class Cal{
+	public static void main(String args[]){
+		JFrame f1=new JFrame();
+		f1.setSize(300,300);
+		f1.setTitle("Calculator");
+		f1.setDefaultCloseOperation(3);//exit on close
+		f1.setLocationRelativeTo(null);
+		f1.setVisible(true);
+	}
+}
