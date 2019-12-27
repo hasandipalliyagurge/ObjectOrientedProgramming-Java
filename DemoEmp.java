58a71e4c41980891fa9f09a8a07a01a1f9d2f9ab
hasandi_palliaguruge <pghasandi@gmail.com>
2019-12-27 18:08:55 +0530

    object

---
+++ DemoEmp.java
@@ -0,0 +1,32 @@
+class Employee{
+	String name;
+	double salary;
+	public void setData(String name, double salary){
+		this.name=name;
+		this.salary=salary;
+	}
+	public void printEmployee(){
+		System.out.println(name+" - "+salary); 
+	}
+}
+
+class Demo{
+	public static void incrementSalary(double salary, double rate){
+		salary*=(100+rate)/100;
+	}
+	public static void incrementSalary(Employee e1, double rate){
+		e1.salary*=(100+rate)/100;
+	}
+	public static void main(String args[]){
+		Employee e1=new Employee();
+		e1.setData("Danapala",10000);
+		e1.printEmployee();
+		
+		Demo.incrementSalary(e1.salary,10);
+		e1.printEmployee();
+		
+		Demo.incrementSalary(e1,10);
+		e1.printEmployee();
+	}
+	
+}
